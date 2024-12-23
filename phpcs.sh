fe4fc855d0cc0df022345a14807bb67552eaaeea
Sergey <i@b9sk.ru>
2024-12-23 12:46:38 +0300

    update phpcs config & its shell script

--- phpcs.sh
+++ phpcs.sh
@@ -1,1 +1,11 @@
-docker compose exec php ./vendor/bin/phpcs
+#!/bin/bash
+
+if [ "$#" -eq 0 ]; then
+    echo "⚠ No arguments supplied, using default with --no-cache"
+    docker compose exec php ./vendor/bin/phpcs --no-cache
+elif [ "$1" == "fix" ]; then
+    echo "Applying fixes..."
+    docker compose exec php ./vendor/bin/phpcbf
+else
+    docker compose exec php ./vendor/bin/phpcs "$@"
+fi
